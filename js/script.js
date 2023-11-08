c52c51a24864aee54e704bddd494a1ccb6e4e38a
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2023-11-08 15:13:04 -0300

    commit sei la
    tem muita coisa inacabada calma

--- js/script.js
+++ js/script.js
@@ -9,19 +9,32 @@ function Add(){
     Estoque.push(produto)
     
     var divNova = document.createElement("div");
+    var divButton = document.createElement("button")
     divNova.id = (ProdAtual + 1)
-    var conteudoNovo = document.createTextNode(divNova.id + "-" + Estoque[ProdAtual] + "; Quantidade: " + EstoqueQuant[ProdAtual]);
+    divButton.id = ("Button" + ProdAtual)
+    var conteudoNovo = document.createTextNode(divNova.id + "-" + Estoque[ProdAtual] + "; Qnt: " + EstoqueQuant[ProdAtual]);
     divNova.appendChild(conteudoNovo)
     var divAtual = document.getElementById("mostruario");
     document.body.insertBefore(divNova, divAtual);
+    document.body.insertBefore(divButton, divAtual);
+    divButton.classList.add("ButtonRemove")
     divNova.classList.add("mostruario")
+    var elemento = document.getElementById("button" + ProdAtual)
+    //var rect = elemento.getBoundingClientRect()
+    //var x = rect.left
+    //var y = rect.top
+    //alert(x)
+    //alert(y)
+    //elemento.style.top = 
     ProdAtual ++
 }
 
 function Remove(){
     var num = prompt("Qual o número do item que você deseja deletar?")
     const div2 = document.getElementById(num);
+    const div3 = document.getElementById("Button" + (num - 1));
     div2.remove();
+    div3.remove()
 }
 
 function Mudar(){
